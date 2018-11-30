d1c8b5a4d2e05f7aa4f6b4804ff2a2a674f4ef3c
DavidMazarro <davidmazarro98@gmail.com>
2018-11-30 11:03:23 +0100

    Changed Inconsolata to Tamzen (yay bitmap fonts)

--- init.el
+++ init.el
@@ -43,11 +43,11 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; Checks if Inconsolata is on the system and uses it as a global font
+;; Checks if Tamzen is on the system and uses it as a global font
 
-(when (member "Inconsolata" (font-family-list))
-  (add-to-list 'default-frame-alist '(font . "Inconsolata-13"))
-  (set-face-attribute 'default t :font "Inconsolata-13"))
+(when (member "Tamzen" (font-family-list))
+  (add-to-list 'default-frame-alist '(font . "Tamzen-15"))
+  (set-face-attribute 'default t :font "Tamzen-15"))
 
 ;; Something to do with backups files lmao
 
@@ -68,7 +68,7 @@
 
 ;; This prevents the cursor from blinking
 
-(blink-cursor-mode nil)
+(blink-cursor-mode 0)
 
 ;; This sets the Mode Line to display not just the line number
 ;; but also the column number (it's not that useful but ¯\_(ツ)_/¯)
@@ -94,17 +94,22 @@
   (exec-path-from-shell-initialize))
   )
 
-(use-package doom-themes
-  :ensure t
-  :config (load-theme 'doom-one t)
-  (doom-themes-neotree-config)
-  (doom-themes-org-config)
-  )
+;; (use-package darkokai-theme
+;;   :ensure t
+;;   :config (load-theme 'darkokai t)
+;;   )
 
-;;(use-package gruvbox-theme
-;;  :ensure t
-;;  :config (load-theme 'gruvbox-dark-soft t)
-;;  )
+;; (use-package doom-themes
+;;   :ensure t
+;;   :config (load-theme 'doom-one t)
+;;   (doom-themes-neotree-config)
+;;   (doom-themes-org-config)
+;;   )
+
+(use-package gruvbox-theme
+  :ensure t
+  :config (load-theme 'gruvbox-dark-soft t)
+  )
 
 ;;(use-package rebecca-theme
 ;;  :ensure t
@@ -212,7 +217,7 @@
    (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
    (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
    (setq reftex-plug-into-AUCTeX t)
-   )
+)
   
 (use-package haskell-mode
   :ensure t
@@ -266,6 +271,7 @@
 (use-package hlinum
   :ensure t
   :init
+  (global-hl-line-mode 1)
   (global-linum-mode 1)
   (hlinum-activate)
   )
@@ -281,6 +287,7 @@
 (use-package spaceline-config
   :init
   (spaceline-all-the-icons-theme)
+  (setq powerline-default-separator 'wave)
   )
 
 (use-package spaceline-all-the-icons
@@ -292,8 +299,7 @@
 (use-package org
   :mode (("\\.org$" . org-mode))
   :config
-  (setq org-pretty-entities t
-	org-src-fontify-natively t
+  (setq org-src-fontify-natively t
 	org-fontify-whole-heading-line t
 	org-fontify-done-headline t
 	org-fontify-quote-and-verse-blocks t)
@@ -329,6 +335,23 @@
        (if (yes-or-no-p "Do you really want to clean all buffers? ")
            (kill-buffers) nil))
 
-(global-set-key (kbd "C-x C-k") 'clean-buffers)
-
-;;; init.el ends here
+(global-set-key (kbd "C-x C-S-k") 'clean-buffers)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a19265ef7ecc16ac4579abb1635fd4e3e1185dcacbc01b7a43cf7ad107c27ced" "3e160974b9e3e1b53270d1fb5bbaf56f0c689017e177972f72584bf096efc4cc" "b9a06c75084a7744b8a38cb48bc987de10d68f0317697ccbd894b2d0aca06d2b" "3cb2d5a795e1c93d1fbc8360d6ea41f0173aa1366d334b16e1b83b996b8d9ce6" "73bff6f2ef60f8a1238a9ca666235d258e3acdeeed85d092ca532788dd7a33c4" "12b204c8fcce23885ce58e1031a137c5a14461c6c7e1db81998222f8908006af" "5f4e4c9f5de8156f964fdf8a1b8f8f659efbfeff88b38f49ce13953a84272b77" "a866134130e4393c0cad0b4f1a5b0dd580584d9cf921617eee3fd54b6f09ac37" "7666b079fc1493b74c1f0c5e6857f3cf0389696f2d9b8791c892c696ab4a9b64" "53d1bb57dadafbdebb5fbd1a57c2d53d2b4db617f3e0e05849e78a4f78df3a1b" "4e21fb654406f11ab2a628c47c1cbe53bab645d32f2c807ee2295436f09103c6" "2af26301bded15f5f9111d3a161b6bfb3f4b93ec34ffa95e42815396da9cb560" "75c5c39809c52d48cb9dcbf1694bf2d27d5f6fd053777c194e0b69d8e49031c0" default)))
+ '(package-selected-packages
+   (quote
+    (firecode-theme moe-theme elpygen darkokai-theme use-package toxi-theme spaceline-all-the-icons smex smartparens rebecca-theme rainbow-mode rainbow-delimiters pretty-mode org-bullets neotree multiple-cursors multi-term monokai-theme magit jedi jdee ido-vertical-mode ido-ubiquitous hlinum helm haskell-mode gruvbox-theme flx-ido flatland-theme expand-region exec-path-from-shell esup ensime elpy doom-themes company-jedi cheatsheet base16-theme avy auctex)))
+ '(spaceline-all-the-icons-separator-type (quote slant))
+ '(spaceline-all-the-icons-separators-invert-direction t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
