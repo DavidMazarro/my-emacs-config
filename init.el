17164d97237b346845f837e9b69c11050a8a0d66
David Mazarro <davidmazarro98@gmail.com>
2019-11-15 12:52:53 +0100

    Added visual-regexp-steroids, commented out neotree

--- init.el
+++ init.el
@@ -132,13 +132,27 @@
   (add-hook 'haskell-mode-hook #'pretty-mode)
   )
 
-(use-package neotree
+(use-package visual-regexp-steroids
   :ensure t
-  :init
-  (setq neo-theme 'icons)
-  (neotree)
-  :bind ("M-n t" . neotree-toggle)
+  :config
+  (define-key global-map (kbd "C-c r") 'vr/replace)
+  (define-key global-map (kbd "C-c q") 'vr/query-replace)
+  ;; if you use multiple-cursors, this is for you:
+  (define-key global-map (kbd "C-c m") 'vr/mc-mark)
+  ;; to use visual-regexp-steroids's isearch instead of the built-in regexp isearch, also include the following lines:
+  ;; (define-key esc-map (kbd "C-r") 'vr/isearch-backward) ;; C-M-r
+  ;; (define-key esc-map (kbd "C-s") 'vr/isearch-forward) ;; C-M-s
+  (define-key global-map (kbd "C-r") 'vr/isearch-backward)
+  (define-key global-map (kbd "C-s") 'vr/isearch-forward)
   )
+
+;; (use-package neotree
+;;   :ensure t
+;;   :init
+;;   (setq neo-theme 'icons)
+;;   (neotree)
+;;   :bind ("M-n t" . neotree-toggle)
+;;   )
 
 (use-package ido
   :ensure
