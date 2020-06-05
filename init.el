ab16ba8fca97eae6b2198156b12ed163e0921490
DavidMazarro <davidmazarro98@gmail.com>
2020-06-05 18:19:23 +0200

    Changes for Ubuntu 20.04

--- init.el
+++ init.el
@@ -97,11 +97,12 @@
 
 (set-face-attribute 'default nil
 		    :family "Hasklig"
-                    :height 150
+                    :height 180
                     :weight 'normal
                     :width 'normal)
 
 (use-package hasklig-mode
+  :ensure t
   :hook (haskell-mode))
 
 
@@ -123,12 +124,12 @@
 
 ;;;;;; use-package declarations ;;;;;;
 
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-  )
+;; (use-package exec-path-from-shell
+;;   :ensure t
+;;   :config
+;;   (when (memq window-system '(mac ns x))
+;;   (exec-path-from-shell-initialize))
+;;   )
 
 (use-package random-splash-image
   :ensure t
@@ -181,19 +182,19 @@
   (setq symon-sparkline-type 'boxed)
   )
 
-(use-package visual-regexp-steroids
-  :ensure t
-  :config
-  (define-key global-map (kbd "C-c r") 'vr/replace)
-  (define-key global-map (kbd "C-c q") 'vr/query-replace)
-  ;; if you use multiple-cursors, this is for you:
-  (define-key global-map (kbd "C-c m") 'vr/mc-mark)
-  ;; to use visual-regexp-steroids's isearch instead of the built-in regexp isearch, also include the following lines:
-  ;; (define-key esc-map (kbd "C-r") 'vr/isearch-backward) ;; C-M-r
-  ;; (define-key esc-map (kbd "C-s") 'vr/isearch-forward) ;; C-M-s
-  (define-key global-map (kbd "C-r") 'vr/isearch-backward)
-  (define-key global-map (kbd "C-s") 'vr/isearch-forward)
-  )
+;; (use-package visual-regexp-steroids
+;;   :ensure t
+;;   :config
+;;   (define-key global-map (kbd "C-c r") 'vr/replace)
+;;   (define-key global-map (kbd "C-c q") 'vr/query-replace)
+;;   ;; if you use multiple-cursors, this is for you:
+;;   (define-key global-map (kbd "C-c m") 'vr/mc-mark)
+;;   ;; to use visual-regexp-steroids's isearch instead of the built-in regexp isearch, also include the following lines:
+;;   ;; (define-key esc-map (kbd "C-r") 'vr/isearch-backward) ;; C-M-r
+;;   ;; (define-key esc-map (kbd "C-s") 'vr/isearch-forward) ;; C-M-s
+;;   (define-key global-map (kbd "C-r") 'vr/isearch-backward)
+;;   (define-key global-map (kbd "C-s") 'vr/isearch-forward)
+;;   )
 
 (use-package rainbow-delimiters
   :ensure t
@@ -330,7 +331,7 @@
   (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
   (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
   ;; if not present, remove structured-haskell-mode hook
-  (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+  ;; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
   )
 
 (use-package elpy
@@ -449,32 +450,5 @@
            (kill-buffers) nil))
 
 (global-set-key (kbd "C-x C-S-k") 'clean-buffers)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("ef07cb337554ffebfccff8052827c4a9d55dc2d0bc7f08804470451385d41c5c" "e95ad48fd7cb77322e89fa7df2e66282ade015866b0c675b1d5b9e6ed88649b4" "614e5089876ea69b515c50b6d7fa0a37eb7ed50fda224623ec49e1c91a0af6a1" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a19265ef7ecc16ac4579abb1635fd4e3e1185dcacbc01b7a43cf7ad107c27ced" "3e160974b9e3e1b53270d1fb5bbaf56f0c689017e177972f72584bf096efc4cc" "b9a06c75084a7744b8a38cb48bc987de10d68f0317697ccbd894b2d0aca06d2b" "3cb2d5a795e1c93d1fbc8360d6ea41f0173aa1366d334b16e1b83b996b8d9ce6" "73bff6f2ef60f8a1238a9ca666235d258e3acdeeed85d092ca532788dd7a33c4" "12b204c8fcce23885ce58e1031a137c5a14461c6c7e1db81998222f8908006af" "5f4e4c9f5de8156f964fdf8a1b8f8f659efbfeff88b38f49ce13953a84272b77" "a866134130e4393c0cad0b4f1a5b0dd580584d9cf921617eee3fd54b6f09ac37" "7666b079fc1493b74c1f0c5e6857f3cf0389696f2d9b8791c892c696ab4a9b64" "53d1bb57dadafbdebb5fbd1a57c2d53d2b4db617f3e0e05849e78a4f78df3a1b" "4e21fb654406f11ab2a628c47c1cbe53bab645d32f2c807ee2295436f09103c6" "2af26301bded15f5f9111d3a161b6bfb3f4b93ec34ffa95e42815396da9cb560" "75c5c39809c52d48cb9dcbf1694bf2d27d5f6fd053777c194e0b69d8e49031c0" default)))
- '(package-selected-packages
-   (quote
-    (visual-regexp-steroids pdf-tools symon hasklig-mode cyberpunk-theme kaolin-themes doom-themes list-unicode-display firecode-theme moe-theme elpygen darkokai-theme use-package toxi-theme spaceline-all-the-icons smex smartparens rebecca-theme rainbow-mode rainbow-delimiters pretty-mode org-bullets neotree multiple-cursors multi-term monokai-theme magit jedi jdee ido-vertical-mode ido-ubiquitous hlinum helm haskell-mode gruvbox-theme flx-ido flatland-theme expand-region exec-path-from-shell esup ensime elpy company-jedi cheatsheet base16-theme avy auctex)))
- '(safe-local-variable-values (quote ((TeX-command-extra-options . "-shell-escape"))))
- '(spaceline-all-the-icons-separator-type (quote slant))
- '(spaceline-all-the-icons-separators-invert-direction t)
- '(symon-monitors
-   (quote
-    (symon-darwin-memory-monitor symon-darwin-cpu-monitor symon-darwin-network-rx-monitor symon-darwin-network-tx-monitor symon-linux-battery-monitor))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
-; @begin(34194479)@ - Do not edit these lines - added automatically!
-;; (if (file-exists-p "/Users/davidmazarro/.ciaoroot/master/ciao_emacs/elisp/ciao-site-file.el")
-;;   (load-file "/Users/davidmazarro/.ciaoroot/master/ciao_emacs/elisp/ciao-site-file.el"))
-; @end(34194479)@ - End of automatically added lines.
 (put 'set-goal-column 'disabled nil)
